6a129aa68564d3ba2b5800d64c662bdc09397b6a
Adrienne <adriennepaquin@gmail.com>
2021-06-15 15:35:01 -0400

    done

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -3,9 +3,14 @@ import React, { useState } from "react";
 function PlantCard({ plant }) {
 
   const [inStock, setInStock] = useState(true)
+  const [newPrice, setNewPrice] = useState("")
 
   const handleStock = () => {
     setInStock(!inStock)
+  }
+
+  const handleNewPrice = () => {
+    console.log("new")
   }
   
   return (
@@ -13,6 +18,7 @@ function PlantCard({ plant }) {
       <img src={plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
       <p>Price: {plant.price}</p>
+      <input type="number" name="price" step="0.01" placeholder={plant.price} value={newPrice} onChange={handleNewPrice}/>
       {inStock ? (
         <button className="primary" onClick={handleStock}>In Stock</button>
       ) : (
